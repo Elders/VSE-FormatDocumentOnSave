65dea45dd5aa4eb0e5a32d2d5b1a38de0e025a4d
nikolaymankov <nikolay.mankov@mentormate.com>
2013-10-28 18:55:06 +0200

    Initial commit. Works with c# files so far

---
+++ src/NMSD.VSE-FormatDocumentOnSave/KeyBindingCommandFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using EnvDTE;
+using Microsoft.VisualStudio.OLE.Interop;
+
+namespace KeyBindingTest
+{
+    internal class KeyBindingCommandFilter : IOleCommandTarget
+    {
+        DTE dte;
+
+        internal bool m_added;
+
+        internal IOleCommandTarget m_nextTarget;
+
+        public KeyBindingCommandFilter(DTE dte)
+        {
+            this.dte = dte;
+        }
+
+        int IOleCommandTarget.Exec(ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
+        {
+            if (pguidCmdGroup == Guid.Parse(Microsoft.VisualStudio.VSConstants.CMDSETID.StandardCommandSet97_string) && nCmdID == (uint)Microsoft.VisualStudio.VSConstants.VSStd97CmdID.SaveProjectItem)
+            {
+                dte.ExecuteCommand("Edit.FormatDocument", string.Empty);
+            }
+            return m_nextTarget.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
+        }
+
+        int IOleCommandTarget.QueryStatus(ref Guid pguidCmdGroup, uint cCmds, OLECMD[] prgCmds, IntPtr pCmdText)
+        {
+            return m_nextTarget.QueryStatus(ref pguidCmdGroup, cCmds, prgCmds, pCmdText);
+        }
+    }
+}
---
+++ src/NMSD.VSE-FormatDocumentOnSave/KeyBindingFilterProvider.cs
@@ -0,0 +1,52 @@
+using System.ComponentModel.Composition;
+using EnvDTE;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Utilities;
+
+namespace KeyBindingTest
+{
+    [Export(typeof(IVsTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Editable)]
+    internal class KeyBindingCommandFilterProvider : IVsTextViewCreationListener
+    {
+        [Import(typeof(IVsEditorAdaptersFactoryService))]
+        internal IVsEditorAdaptersFactoryService editorFactory = null;
+
+        [Import(typeof(SVsServiceProvider))]
+        internal SVsServiceProvider ServiceProvider = null;
+
+        public void VsTextViewCreated(IVsTextView textViewAdapter)
+        {
+            IWpfTextView textView = editorFactory.GetWpfTextView(textViewAdapter);
+            if (textView == null)
+                return;
+
+            var dte = (DTE)ServiceProvider.GetService(typeof(DTE));
+            AddCommandFilter(textViewAdapter, new KeyBindingCommandFilter(dte));
+        }
+
+        void AddCommandFilter(IVsTextView viewAdapter, KeyBindingCommandFilter commandFilter)
+        {
+            if (commandFilter.m_added == false)
+            {
+                //get the view adapter from the editor factory
+                IOleCommandTarget next;
+                int hr = viewAdapter.AddCommandFilter(commandFilter, out next);
+
+                if (hr == VSConstants.S_OK)
+                {
+                    commandFilter.m_added = true;
+                    //you'll need the next target for Exec and QueryStatus 
+                    if (next != null)
+                        commandFilter.m_nextTarget = next;
+                }
+            }
+        }
+    }
+}
