b5e1ecbda6f5181abca69fb143ad2b2480d11f20
Emmanuel Counasse <manuc66@gmail.com>
2021-02-26 01:05:39 +0100

    fix potential error when parsing, if value is not recognized, keep the current value

--- src/Elders.VSE-FormatDocumentOnSave/Configurations/EditorConfigConfiguration.cs
+++ src/Elders.VSE-FormatDocumentOnSave/Configurations/EditorConfigConfiguration.cs
@@ -14,14 +14,11 @@ namespace Elders.VSE_FormatDocumentOnSave.Configurations
 
         public EditorConfigConfiguration(string formatConfigFile)
         {
-            var parser = new EditorConfig.Core.EditorConfigParser(formatConfigFile);
+            var parser = new EditorConfigParser(formatConfigFile);
             FileConfiguration configFile = parser.Parse(formatConfigFile).First();
 
-            if (configFile.Properties.ContainsKey("enable"))
-            {
-                configFile.Properties.TryGetValue("enable", out string enableAsString);
-                bool.TryParse(enableAsString, out enable);
-            }
+            if (configFile.Properties.TryGetValue("enable", out string enableAsString) && bool.TryParse(enableAsString, out bool enableParsed))
+                enable = enableParsed;
 
             if (configFile.Properties.ContainsKey("allowed_extensions"))
                 configFile.Properties.TryGetValue("allowed_extensions", out allowed);
@@ -32,11 +29,8 @@ namespace Elders.VSE_FormatDocumentOnSave.Configurations
             if (configFile.Properties.ContainsKey("command"))
                 configFile.Properties.TryGetValue("command", out command);
 
-            if (configFile.Properties.ContainsKey("enable_in_debug"))
-            {
-                configFile.Properties.TryGetValue("enable_in_debug", out string enableInDebugAsString);
-                bool.TryParse(enableInDebugAsString, out enableInDebug);
-            }
+            if (configFile.Properties.TryGetValue("enable_in_debug", out string enableInDebugAsString) && bool.TryParse(enableInDebugAsString, out bool enableInDebugParsed))
+                enableInDebug = enableInDebugParsed;
         }
 
         public bool IsEnable => enable;
