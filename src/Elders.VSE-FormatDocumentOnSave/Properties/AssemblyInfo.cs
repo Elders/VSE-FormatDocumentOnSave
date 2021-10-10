8d015aa37daa0e100baa744bc3d9659b03765774
Bar Nuri <barn@ke-la.com>
2021-10-10 16:50:55 +0300

    upgrade csproj

--- src/Elders.VSE-FormatDocumentOnSave/Properties/AssemblyInfo.cs
+++ src/Elders.VSE-FormatDocumentOnSave/Properties/AssemblyInfo.cs
@@ -3,31 +3,5 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Format Document On Save")]
-[assembly: AssemblyDescription("Enables auto formatting of the code when you save a file. Visual Studio supports auto formatting of the code with the CTRL+E,D or CTRL+E,F key shortcuts but with this extension the VS command 'Format Document' is executed on Save.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Elders")]
-[assembly: AssemblyProduct("Elders.VSE")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
-[assembly: NeutralResourcesLanguage("en-US")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
